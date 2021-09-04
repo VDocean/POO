553131d19a568752a0e14d93862fe329896d6fd0
Vitória Vasconcelos do Nascimento <87788208+VDocean@users.noreply.github.com>
2021-09-04 11:00:17 -0300

    Add files via upload

---
+++ Nitendo/Mario.java
@@ -0,0 +1,23 @@
+package Nitendo;
+
+public class Mario {
+     String Profissao;
+     String reside;
+     double altura;
+     
+     void Comportamento(){
+    	 System.out.println("Profissão: "+this.Profissao);
+    	 System.out.println("Mora em: "+ this.reside);
+    	 System.out.println("Sua altura é: "+this.altura);
+     }
+     
+     void SuperPoderes() {
+    	 System.out.println("Super pulo!");
+    	 System.out.println("Força sobre-humana.");
+    	 System.out.println("Hábil motorista");
+     }
+   
+ 
+     
+}
+
---
+++ Nitendo/SuperHeroi.java
@@ -0,0 +1,12 @@
+package Nitendo;
+
+public class SuperHeroi {
+ public static void main(String [] args) {
+	 Mario persona1= new Mario();
+	 persona1.Profissao="Encanador";
+	 persona1.reside="Mario Land";
+	 persona1.altura= 1.36;
+	 persona1.Comportamento();
+	 persona1.SuperPoderes();
+ }
+}
